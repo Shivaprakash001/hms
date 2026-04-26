8107be281037a7cb5363d329f0f715f679a50894
Shivaprakash001 <spchidiri2006@gmail.com>
2026-04-26 15:39:04 +0530

    fix: set pdfkit as an external package in Next.js config to preserve font assets (*.afm) on Vercel

--- backend-next/next.config.mjs
+++ backend-next/next.config.mjs
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config({ path: path.resolve(process.cwd(), "..", ".env") });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['pdfkit'],
+    }
+};
 
 export default nextConfig;
